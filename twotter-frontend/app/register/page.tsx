4454d40cf99866fef780e00281421ab7773246da
widua <mikolaj.widla@gmail.com>
2025-06-22 22:58:02 +0200

    Added month, and day handling to register

--- twotter-frontend/app/register/page.tsx
+++ twotter-frontend/app/register/page.tsx
@@ -1,8 +1,17 @@
+"use client"
 import Modal from "@/components/ui/modal"
+import { useState } from 'react'
 
 export default function Register() {
 	const months: readonly string[] = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 	const daysInMonth = (year: number, month: number) => new Date(year, month, 0).getDate()
+	const [birthDate, setDate] = useState(new Date())
+
+	function dateUpdater(month: string, day: number, year: number) {
+		console.log(month)
+		console.log(months.indexOf(month))
+		setDate(new Date(year, months.indexOf(month) + 1, day))
+	}
 	return (
 		<Modal>
 			<div>
@@ -13,19 +22,24 @@ export default function Register() {
 				<p className="text-sm text-gray-400">This will not be shown publicly. Confirm your own age, even if this account is for business, a pet, or something else</p>
 
 				<div className="flex flex-row mt-8">
-					<select name="monthSelector" defaultValue="" className="p-2 mt-2 rounded-l bg-black border-2 border-gray-700 border-solid text-center text-white">
+					<select name="monthSelector" onChange={e => dateUpdater(e.target.value, birthDate.getDay(), birthDate.getFullYear())} defaultValue="" className="p-2 mt-2 rounded-l bg-black border-2 border-gray-700 border-solid text-center text-white">
 						<option className="text-start" value="" defaultChecked hidden disabled>Month</option>
 						{
 							months.map((month: string) => (
-								<option className="text-start" key={month} value={month.toLowerCase()}>{month}</option>
+								<option className="text-start" key={month} value={month}>{month}</option>
+							))
+						}
+					</select>
+					<select defaultValue={0} onChange={e => dateUpdater(months[birthDate.getMonth() - 1], Number(e.target.value), birthDate.getFullYear())} className="p-2 mt-2 rounded-l bg-black border-2 border-gray-700 border-solid text-center text-white" >
+						<option className="text-start" value={0} defaultChecked hidden disabled>Day</option>
+						{
+							Array.from({ length: daysInMonth(birthDate.getFullYear(), birthDate.getMonth()) }, (_, i) => i + 1).map((day: number) => (
+								<option className="text-start" key={day} value={day}>{day}</option>
 							))
 						}
 					</select>
 					<select defaultValue="" className="p-2 mt-2 rounded-l bg-black border-2 border-gray-700 border-solid text-center text-white" >
-						<option className="text-start" value="" defaultChecked hidden disabled>Day</option>
-						{
-
-						}
+						<option className="text-start" value="" defaultChecked hidden disabled>Year</option>
 					</select>
 				</div>
 			</div>
